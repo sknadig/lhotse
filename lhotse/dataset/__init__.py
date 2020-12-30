ec1ccb31ee4c25664fc5a6ca144b34448a0fb8ad
Piotr Żelasko <petezor@gmail.com>
2020-12-29 21:54:42 -0500

    More work on fields configuration; ported ASR, TTS and VAD examples to use the proposed scheme

--- lhotse/dataset/__init__.py
+++ lhotse/dataset/__init__.py
@@ -1,11 +1,10 @@
+from .core import SpeechDataset
+from . import fields
 from .diarization import DiarizationDataset
 from .source_separation import (DynamicallyMixedSourceSeparationDataset, PreMixedSourceSeparationDataset,
                                 SourceSeparationDataset)
 from .speech_recognition import (
-    K2DataLoader,
-    K2SpeechRecognitionDataset,
     K2SpeechRecognitionIterableDataset,
-    SpeechRecognitionDataset,
 )
 from .speech_synthesis import SpeechSynthesisDataset
 from .unsupervised import (DynamicUnsupervisedDataset, UnsupervisedDataset, UnsupervisedWaveformDataset)
